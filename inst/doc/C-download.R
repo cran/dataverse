abb95a966035d318012026b55b2f964bc9f556b9
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2022-06-11 06:20:10 +0000

    version 0.3.11

--- inst/doc/C-download.R
+++ inst/doc/C-download.R
@@ -6,50 +6,59 @@ knitr::opts_chunk$set(results = "hold")
 Sys.setenv("DATAVERSE_SERVER" = "dataverse.harvard.edu")
 
 ## ---------------------------------------------------------------------------------------------------------------------
-Sys.setenv("DATAVERSE_KEY" = "")
-
-## ---------------------------------------------------------------------------------------------------------------------
 library("dataverse")
 library("tibble") # to see dataframes in tidyverse-form
 
-## ---------------------------------------------------------------------------------------------------------------------
-str(dataset_metadata("10.7910/DVN/ARKOTI"), 2)
-
-## ---- messages=FALSE--------------------------------------------------------------------------------------------------
+## ---- echo=FALSE, message=FALSE,include=FALSE-------------------------------------------------------------------------
 energy <- get_dataframe_by_name(
-  "comprehensiveJapanEnergy.tab",
-  "10.7910/DVN/ARKOTI")
+  filename = "comprehensiveJapanEnergy.tab",
+  dataset = "10.7910/DVN/ARKOTI", 
+  server = "dataverse.harvard.edu")
 
+## ---- eval=FALSE------------------------------------------------------------------------------------------------------
+#  energy <- get_dataframe_by_name(
+#    filename = "comprehensiveJapanEnergy.tab",
+#    dataset = "10.7910/DVN/ARKOTI",
+#    server = "dataverse.harvard.edu")
+
+## ---------------------------------------------------------------------------------------------------------------------
 head(energy)
 
 ## ---------------------------------------------------------------------------------------------------------------------
 library(readr)
 energy <- get_dataframe_by_name(
-  "comprehensiveJapanEnergy.tab",
-  "10.7910/DVN/ARKOTI",
+  filename = "comprehensiveJapanEnergy.tab",
+  dataset = "10.7910/DVN/ARKOTI", 
+  server = "dataverse.harvard.edu",
   .f = function(x) read.delim(x, sep = "\t"))
 
 head(energy)
 
 ## ----  message=FALSE--------------------------------------------------------------------------------------------------
 argentina_tab <- get_dataframe_by_name(
-  "alpl2013.tab",
-  "10.7910/DVN/ARKOTI")
+  filename = "alpl2013.tab",
+  dataset = "10.7910/DVN/ARKOTI",
+  server = "dataverse.harvard.edu")
 
 ## ---------------------------------------------------------------------------------------------------------------------
 str(argentina_tab$polling_place)
 
 ## ---------------------------------------------------------------------------------------------------------------------
 argentina_dta <- get_dataframe_by_name(
-  "alpl2013.tab",
-  "10.7910/DVN/ARKOTI",
+  filename = "alpl2013.tab",
+  dataset = "10.7910/DVN/ARKOTI",
+  server = "dataverse.harvard.edu",
   original = TRUE,
   .f = haven::read_dta)
 
 ## ---------------------------------------------------------------------------------------------------------------------
 str(argentina_dta$polling_place)
 
+## ---------------------------------------------------------------------------------------------------------------------
+str(dataset_metadata("10.7910/DVN/ARKOTI", server = "dataverse.harvard.edu"), 
+    max.level = 2)
+
 ## ---- eval = FALSE----------------------------------------------------------------------------------------------------
-#  code3 <- get_file("chapter03.R", "doi:10.7910/DVN/ARKOTI")
+#  code3 <- get_file("chapter03.R", "doi:10.7910/DVN/ARKOTI", server = "dataverse.harvard.edu")
 #  writeBin(code3, "chapter03.R")
 
